0e7468e768030a3620d360a15e0b3341fd8c08f5
Karl <karl.mullr@gmail.com>
2024-09-07 20:57:47 +0200

    feat: :sparkles: add the Role factory to be used in test

---
+++ src/database/factory/RoleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Mimachh\Guardians\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
+ */
+class RoleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+        ];
+    }
+}
