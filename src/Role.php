7081e6459b8adc617afc734a99736a931dd10133
Karl <karl.mullr@gmail.com>
2024-09-07 21:35:35 +0200

    fix: :bug: add the use HasFactory to the role model

--- src/Role.php
+++ src/Role.php
@@ -3,10 +3,12 @@
 namespace Mimachh\Guardians;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    use HasFactory;
     protected $fillable = ['name', 'slug'];
 
     public function users()
